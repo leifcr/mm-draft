936d39e485591bc495282c50f4905adc510cca4d
Leif Ringstad <leifcr@gmail.com>
2011-12-08 14:48:34 +0100

    Fixed bug with multiple published records when republising

--- lib/mongo_mapper/plugins/draft.rb
+++ lib/mongo_mapper/plugins/draft.rb
@@ -2,92 +2,105 @@
 require 'mongo_mapper'
 
 module MongoMapper
-	module Plugins
-		module Draft
- 		  extend ActiveSupport::Concern
-		  # ClassMethods module will automatically get extended
+  module Plugins
+    module Draft
+      extend ActiveSupport::Concern
+      # ClassMethods module will automatically get extended
 
-		  included do
-		    key :position, Integer, :default => 1
-		  end		  
+      included do
+        key :position, Integer, :default => 1
+      end     
 
-		  included do
-		    #puts "Configuring #{model}..."
-		    key :draft, Boolean, :default => true
-			key :draft_record_published_id, ObjectId # points to the live page version if draft == true. else NIL
-			# Before destroy callback to remove published record
-			before_destroy :unpublish
-		  end
+      included do
+        #puts "Configuring #{model}..."
+        key :draft, Boolean, :default => true
+      key :draft_record_published_id, ObjectId # points to the live page version if draft == true. else NIL
+      # Before destroy callback to remove published record
+      before_destroy :unpublish
+      end
 
-		  # module ClassMethods
-		  # end
-		
-		  # InstanceMethods module will automatically get included
-		  module InstanceMethods
+      # module ClassMethods
+      # end
+    
+      # InstanceMethods module will automatically get included
+      module InstanceMethods
 
-				def draft?
-					self.draft
-				end
-				
-				def published?
-					if draft?
-	    			if (self.class.find(self.draft_record_published_id) != nil)
-	    				return true
-  					end
-    			else
-    				return true
-					end
-					false
-				end
-		    
-		    def publish
-		    	if (draft? == false) # don't publish non-drafts... 
-		    		return false
-	    		end
-	    		
-		    	if (self.changed?) # save any changes, in case publish is called directly instead of save
-		    		if (self.save == false)
-		    			return false
-		    		end
-	    		end
-	    		
-	    		live_record = self.clone
-	    		
-		    	self.draft_record_published_id = live_record._id
-		    	self.save!
-    			
-	      		live_record.draft = false;
-		    	live_record.save!
-		    	return true
-	    	end
-	    	
-	    	def published_record
-	    		if draft?
-	    			self.class.find(self.draft_record_published_id)
-    			else
-    				self
-  				end
-    		end
-    		
-    		def draft_record
-    			if draft?
-    				self
-  				else
-  					self.class.all(:conditions => { :draft => true, :draft_record_published_id => self._id }).first
-				end
-  			end
-    		
-    		def unpublish
-    			if draft?
-	    			published = self.class.find(self.draft_record_published_id)
-	    			if (published != nil)
-	    				published.destroy
-	  				end
-				end
-  			end
-  			
-		  end # InstanceMethods
-		  
-		end
-	end
+        def draft?
+          self.draft
+        end
+        
+        def published?
+          if draft?
+            if (self.class.find(self.draft_record_published_id) != nil)
+              return true
+            end
+          else
+            return true
+          end
+          false
+        end
+        
+        def publish
+          if (draft? == false) # don't publish non-drafts... 
+            return false
+          end
+          
+          if (self.changed?) # save any changes, in case publish is called directly instead of save
+            if (self.save == false)
+              return false
+            end
+          end
+          # if already published, clone onto existing record..
+          if (self.published?)
+            live_record = self.clone
+            live_record._id = self.published_record_id
+          else
+            live_record = self.clone
+          end
+          
+          self.draft_record_published_id = live_record._id
+          self.save!
+          
+          live_record.draft = false;
+          live_record.save!
+          return true
+        end
+        
+        def published_record
+          if draft?
+            self.class.find(self.draft_record_published_id)
+          else
+            self
+          end
+        end
+
+        def published_record_id
+          if draft?
+            self.draft_record_published_id
+          else
+            self._id
+          end
+        end
+        
+        def draft_record
+          if draft?
+            self
+          else
+            self.class.all(:conditions => { :draft => true, :draft_record_published_id => self._id }).first
+        end
+        end
+        
+        def unpublish
+          if draft?
+            published = self.class.find(self.draft_record_published_id)
+            if (published != nil)
+              published.destroy
+            end
+        end
+        end
+        
+      end # InstanceMethods
+      
+    end
+  end
 end
